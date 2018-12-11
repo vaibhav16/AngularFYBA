22052353029a934de1b4f6db959c3794ce936aa2
vaibhav sharma <vaibhav16694@gmail.com>
2018-12-11 17:27:58 +0530

    11/12

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,4 +1,8 @@
+// export class Constants {
+//     public static get apiURL(): string { return "https://mobile.folsomyouthbasketball.com"; };
+//     public static get testURL(): string { return "https://fybaservice.sapplesystems.com"; };
+//   }
+
 export class Constants {
-    public static get apiURL(): string { return "https://mobile.folsomyouthbasketball.com"; };
-    public static get testURL(): string { return "https://fybaservice.sapplesystems.com"; };
-  }
+  public static get apiURL(): string { return "https://fybaservice.sapplesystems.com"; };
+}
--- src/app/official/profile/profile.component.ts
+++ src/app/official/profile/profile.component.ts
@@ -26,13 +26,31 @@ export class ProfileComponent implements OnInit {
   
 
   processFile(imageInput:any){
-    for(var i = 0; i < imageInput.files.length; i++) {     
-      if (imageInput.files[i]) {
-        this.formData.append(name, imageInput.files[i], imageInput.files[i].name);
-        this.uploadedFiles.push(imageInput.files[i]);
-        console.log(this.formData);  
-      }
-    }
+
+    const formData =  new FormData();    
+    if(imageInput){
+      console.log(imageInput.files[0]);
+      formData.append('UserId', this.loginService.userId.toString());
+      formData.append('SessionKey',this.loginService.sessionKey);
+      formData.append('SeasonId',this.loginService.seasonId);
+      formData.append('LeagueId',this.loginService.leagueId);
+      formData.append('Page','Profile');
+      formData.append('Files',imageInput.files[0], imageInput.files[0].name);
+         
+     
+    
+      this.officialService.uploadProfileImage(formData);
+
+    }  
+   
+    // for(var i = 0; i < imageInput.files.length; i++) {     
+    //   if (imageInput.files[i]) {
+    //     this.formData.append(name, imageInput.files[i], imageInput.files[i].name);
+    //     this.uploadedFiles.push(imageInput.files[i]);
+    //     console.log(this.formData);  
+    //   }
+    // }
+
       
   }
 
