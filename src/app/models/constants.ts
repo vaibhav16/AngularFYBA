1df964bda9d03d8518b3f0adddb30e387c4501fd
vaibhav sharma <vaibhav16694@gmail.com>
2018-12-18 18:22:53 +0530

    18/12

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -4,6 +4,6 @@
 //   }
 
 export class Constants {
-  //public static get apiURL(): string { return "https://fybaservice.sapplesystems.com"; };
-  public static get apiURL(): string { return "https://mobile.folsomyouthbasketball.com"; };
+  public static get apiURL(): string { return "https://fybaservice.sapplesystems.com"; };
+  //public static get apiURL(): string { return "https://mobile.folsomyouthbasketball.com"; };
 }
