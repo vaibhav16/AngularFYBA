ef85dd16cd13edce8d52e6b5b510fd2404503d32
vaibhav sharma <vaibhav16694@gmail.com>
2018-12-25 03:40:57 +0530

    pw validation

--- src/app/common/changepassword/changepassword.component.ts
+++ src/app/common/changepassword/changepassword.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit,TemplateRef,Directive, Input } from '@angular/core';
 import { ChangePw } from '../models/changepw.model';
 //import { Http, Response, Headers, RequestOptions, RequestMethod,JSONPConnection } from '@angular/http';
 //import { NgForm } from '@angular/forms';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators,FormControl } from '@angular/forms';
 import { PasswordValidation  } from './confirm-password.validator';
 import { FinalFilter } from '../../official/classes/selectgame/finalFilter.model';
 import { LoginService } from './../services/login.service';
@@ -20,6 +20,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class ChangepasswordComponent implements OnInit {
   headerImg:string;
+  pass:PasswordValidation;
 
   apiModel: FinalFilter={
     UserID:'',
@@ -50,29 +51,54 @@ export class ChangepasswordComponent implements OnInit {
 
     ngOnInit() {
       this.headerImg = 'official_header_img';
-        this.pwFormGroup = this.formBuilder.group({
-                currentPassword:['', RxwebValidators.minLength({value:1 })], 
-                  newPassword:['',
-                  RxwebValidators.password({validation:{maxLength: 10,minLength: 1} })], 
-        confirmPassword:['', RxwebValidators.compare({fieldName:'newPassword' })], 
-                                });
-    }
+      this.pwFormGroup = new FormGroup({
+        currentPassword: new FormControl('', [Validators.required, Validators.minLength(1), 
+          this.checkPassword]),
+        newPassword: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        confirmPassword: new FormControl('', [Validators.required, Validators.minLength(2)])
+      },
+      this.passwordsShouldMatch
+    );
+  }
 
+  get currentPassword() { return this.pwFormGroup.get('currentPassword'); }
+
+  get newPassword() { return this.pwFormGroup.get('newPassword'); }
+
+  get confirmPassword() { return this.pwFormGroup.get('confirmPassword'); }
+
+  private checkPassword(control: FormControl) {
+    return control.value.toString().length >= 1 && control.value.toString().length <= 10
+      ? null
+      : {'outOfRange': true};
+  }
+
+  private passwordsShouldMatch(fGroup: FormGroup) {
+    return fGroup.get('newPassword').value === fGroup.get('confirmPassword').value
+      ? null : {'mismatch': true};
+  }
+  // this.pwFormGroup = this.formBuilder.group({
+  //               currentPassword:['', RxwebValidators.minLength({value:1 })], 
+  //                 newPassword:['',
+  //                 RxwebValidators.password({validation:{maxLength: 10,minLength: 1} })], 
+  //       confirmPassword:['', RxwebValidators.compare({fieldName:'newPassword' })], 
+  //                               });
+  
   modalRef:BsModalRef;
   onSubmit(template: TemplateRef<any>) {
     this.submitted=true;
 
-    console.log(this.pwFormGroup.value);
-    this.changePwModel.Email = this.cookieService.get('email').toString();
-    this.changePwModel.OldPassword = this.pwFormGroup.value.currentPassword;
-    this.changePwModel.NewPassword= this.pwFormGroup.value.newPassword;
-    this.changePwModel.ConfirmPassword = this.pwFormGroup.value.confirmPassword;
-    this.apiModel.RequestedData = JSON.stringify(this.changePwModel);
-    this.apiModel.SessionKey = this.loginService.sessionKey;
-    this.apiModel.UserID =  this.loginService.userId.toString();
-    this.changePwService.changePassword(JSON.stringify(this.apiModel)).then(res=>{
-      this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-    });
+     console.log(this.pwFormGroup.value);
+    // this.changePwModel.Email = this.cookieService.get('email').toString();
+    // this.changePwModel.OldPassword = this.pwFormGroup.value.currentPassword;
+    // this.changePwModel.NewPassword= this.pwFormGroup.value.newPassword;
+    // this.changePwModel.ConfirmPassword = this.pwFormGroup.value.confirmPassword;
+    // this.apiModel.RequestedData = JSON.stringify(this.changePwModel);
+    // this.apiModel.SessionKey = this.loginService.sessionKey;
+    // this.apiModel.UserID =  this.loginService.userId.toString();
+    // this.changePwService.changePassword(JSON.stringify(this.apiModel)).then(res=>{
+    //   this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+    // });
    }
 
    hideModal(){        
--- src/app/common/changepassword/confirm-password.validator.ts
+++ src/app/common/changepassword/confirm-password.validator.ts
@@ -1,45 +1,16 @@
 import { AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class PasswordValidation {
-
-    static MatchPassword(AC: AbstractControl) {
-       let password = AC.get('password').value; // to get value in input tag
-       let confirmPassword = AC.get('confirmPassword').value; // to get value in input tag       
-       let oldPassword = AC.get('oldPassword').value; // to get value in input tag
-
-       if(password != confirmPassword) {
-            //console.log('false');
-            AC.get('confirmPassword').setErrors( {MatchPassword: true} )
-        } else if(oldPassword.length<=0||confirmPassword.length<=0||password.length<=0){
-            AC.get('oldPassword').setErrors({EmptyField: true})                   
-           
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+        return (group: FormGroup) => {
+          let passwordInput = group.controls[passwordKey],
+              passwordConfirmationInput = group.controls[passwordConfirmationKey];
+          if (passwordInput.value !== passwordConfirmationInput.value) {
+            return passwordConfirmationInput.setErrors({notEquivalent: true})
+          }
+          else {
+              return passwordConfirmationInput.setErrors(null);
+          }
         }
-        else{
-            return null
-        }
-    }
-
-    // static LengthError(AC: AbstractControl) {
-    //     let password = AC.get('password').value; // to get value in input tag
-    //     let confirmPassword = AC.get('confirmPassword').value; // to get value in input tag       
-    //     let oldPassword = AC.get('oldPassword').value; // to get value in input tag
- 
-    //     if(password.length<1) {
-    //          //console.log('false');
-    //          AC.get('password').setErrors( {LengthError: true} )
-    //      } else if(oldPassword.length<1){
-    //          AC.get('oldPassword').setErrors({LengthError: true})                  
-    //      }
-    //      else if(confirmPassword.length<1){
-    //          AC.get('oldPassword').setErrors({LengthError: true})                  
-    //      }
-    //      else{
-    //          return null
-    //      }
-    //  }
-
-    // static EmptyField(AC: AbstractControl) {
-    //     let password = AC.get('password').value; // to get value in input tag
-    //     let confirmPassword = AC.get('confirmPassword').value; // to get value in input tag
-            
-    //  }
+      }
 }
