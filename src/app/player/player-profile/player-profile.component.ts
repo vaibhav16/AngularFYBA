e3d5aba0acfffeede303c27c55ba8770306fef88
shashank garg <shashank.garg@sapple.co.in>
2019-07-17 10:59:43 +0530

    17/7

--- src/app/player/player-profile/player-profile.component.ts
+++ src/app/player/player-profile/player-profile.component.ts
@@ -22,7 +22,9 @@ export class PlayerProfileComponent implements OnInit {
 
   fetchingData: boolean;
   profileForm;
-
+  
+  dropdownList = [];
+  dropdownSettings = {};
   constructor(public playerService: PlayerService,
     public router: Router, private fb: FormBuilder,
     private modalService: BsModalService,
@@ -45,8 +47,9 @@ export class PlayerProfileComponent implements OnInit {
           email: new FormControl(this.parentInfo[i]["Parent_Email"],[Validators.email, Validators.required]),          
           homePhone: new FormControl(this.parentInfo[i]["Parent_HomePhone"],[ Validators.pattern(/^[- +()]*[0-9][- +()0-9]*$/),Validators.minLength(7), Validators.maxLength(14)]),          
           mobilePhone:new FormControl(this.parentInfo[i]["Parent_MobilePhone"],[ Validators.pattern(/^[- +()]*[0-9][- +()0-9]*$/),Validators.minLength(7),Validators.maxLength(14)]), 
-          workPhone:new FormControl(this.parentInfo[i]["Parent_WorkPhone"],[ Validators.pattern(/^[- +()]*[0-9][- +()0-9]*$/),Validators.minLength(7),Validators.maxLength(14)])
-         
+          workPhone:new FormControl(this.parentInfo[i]["Parent_WorkPhone"],[ Validators.pattern(/^[- +()]*[0-9][- +()0-9]*$/),Validators.minLength(7),Validators.maxLength(14)]),
+          textingoption:null,
+          Volunteeredposition:null  
         })
       )
     }
@@ -108,6 +111,20 @@ export class PlayerProfileComponent implements OnInit {
     //     this.fetchingData=false;
     //   }
     // )
+    this.dropdownList = [
+      {"id":1,"itemName":"Email"},
+      {"id":2,"itemName":"Home Phone"},
+      {"id":3,"itemName":"Mobile Phone"},    
+    ];
+    
+this.dropdownSettings = { 
+        singleSelection: false, 
+        text:"Select Texting",
+        selectAllText:'Select All',
+        unSelectAllText:'UnSelect All',
+        enableSearchFilter: true,
+        classes:"myclass custom-class"
+      }; 
   }
 
   validEmail: boolean;
