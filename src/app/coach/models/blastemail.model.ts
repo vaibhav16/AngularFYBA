8903ca58a8f08be8bae20e00e17742ab0f27bba9
shashank garg <shashank.garg@sapple.co.in>
2019-07-18 16:19:29 +0530

    18/7

---
+++ src/app/coach/models/blastemail.model.ts
@@ -0,0 +1,28 @@
+export class IEmail{
+    UserID:number;
+    SessionKey:string;
+    RequestedData:string;
+    constructor(){
+        this.UserID = null;
+        this.SessionKey = '';
+        this.RequestedData = '';
+        
+    }
+}
+export class RequestedData{
+    ToEmailIds:string;
+    FromEmailId:string;
+    Subject:string;
+    Body:string;
+    SeasonId:string;
+    LeagueId:string;
+
+    constructor(){
+        this.ToEmailIds = '';
+        this.FromEmailId = '';
+        this.Subject = '';
+        this.Body = '';
+        this.SeasonId = '';
+        this.LeagueId = '';
+    }
+}
