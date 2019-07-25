340c4c008ed307c3f61bffe70d82ccd71dde4a08
princegjr <princegjr@gmail.com>
2019-07-25 14:48:19 +0530

    25/07/2019

--- src/app/common/changepassword/confirm-password.validator.ts
+++ src/app/common/changepassword/confirm-password.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from "@angular/forms";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class PasswordValidation {
   static MatchPassword(AC: AbstractControl) {
-    let password = AC.get("currentPassword").value; // to get value in input tag
+    
     let newPassword = AC.get("newPassword").value; // to get value in input tag
     let confirmPassword = AC.get("confirmPassword").value; // to get value in input tag
 
