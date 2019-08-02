f4dc453ca57e0559c588ae2c185e498e170979b5
shashank garg <shashank.garg@sapple.co.in>
2019-08-02 11:28:41 +0530

    2/8

--- src/app/player/player-calendar/player-calendar.component.ts
+++ src/app/player/player-calendar/player-calendar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { PlayerService } from './../player.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Form, FormArray, Validators, FormControl } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ErrorModalComponent } from './../../common/error-modal/error-modal.component';
+import { DataSharingService } from './../../data-sharing.service';
 
 @Component({
   selector: 'app-player-calendar',
@@ -6,8 +12,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-calendar.component.css']
 })
 export class PlayerCalendarComponent implements OnInit {
+  modalRef: BsModalRef;
+  /**variable declaration start */
 
-  constructor() { }
+    dataRequest: boolean;
+    initialFetchError = null;
+    errorMsg: string;
+  /**variable declaration end */
+  constructor(public modalService: BsModalService,
+    public playerService: PlayerService,
+    public dss: DataSharingService,
+    public router: Router, 
+    private fb: FormBuilder) { }
 
   ngOnInit() {
   }
